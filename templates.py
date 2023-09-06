be09f6f329593287320fef25cfbeffee88821b18
Adrian Chan <adrianpychann@gmail.com>
2023-09-06 17:49:59 +0800

    all files

---
+++ templates.py
@@ -0,0 +1,9 @@
+templates_ID = {
+    "ScratchJr": "1ogSX7tOWXBENUr_L6HXeOcwHEETps3EPDcQu_743_5U",
+    "Scratch": "1FAzAzaAPp11oCpCkKDAxluq_lQjJut4rkmkc30vlUgg",
+    "App Inventor": "1WEHIT61sMeo02B97nk8p9JSh_yUlHrJHUZGWoNf9yYs",
+    "Full Stack": "1ajX3G3GCiPmhEXc9Q8zYjvypC15fMH8fpoEV1ZImBW4",
+    "Full Stack Development Web (Youth)": "1ajX3G3GCiPmhEXc9Q8zYjvypC15fMH8fpoEV1ZImBW4",
+    "Data Science": "1MVpzRx9bCVSEBp3tuHeBCXExLwyo9S7xgXD9kv1oQwA",
+    "Roblox 1:1 - Beginner": "1qEqTz8OdUk1cyuL4y6_51qdJvXoZiiHl",
+}
